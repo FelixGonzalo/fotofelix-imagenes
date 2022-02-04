f83808cb6dc26b8605f791b847b7f4e870924ace
Gonzalo Castro Cubas <felixgonzalocastro@gmail.com>
2022-02-03 23:02:42 -0500

    FIX header

--- src/pages/admin/Layout/index.js
+++ src/pages/admin/Layout/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../../../components/layout/Header'
+import Header from '../../../components/layout/header'
 
 const Layout = () => {
   return (
